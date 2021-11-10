1141009e61e72ef69158345d19f575878c6fb228
z89 <n10234977@qut.edu.au>
2021-11-11 02:12:45 +1000

    get(); add(); cat(); write(); functions implemented (works only on files)

--- main.go
+++ main.go
@@ -2,58 +2,80 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
+	"io"
+	"io/fs"
 	"io/ioutil"
 	"os"
 
 	shell "github.com/ipfs/go-ipfs-api"
-	files "github.com/ipfs/go-ipfs-files"
 )
 
-// get a file or directory from unixfs
-func getUnixfsNode(path string) (files.Node, error) {
-	st, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-	f, err := files.NewSerialFile(path, false, st)
-	if err != nil {
-		return nil, err
-	}
-	return f, nil
-}
-
-func main() {
-	sh := shell.NewShell("localhost:5001")
-
-	file, err := os.Open("./hello")
-	if err != nil {
-		panic(fmt.Errorf("failed: %s", err))
-	}
-
-	reader := bufio.NewReader(file)
-
-	cid, err := sh.Add(reader)
+func add(sh *shell.Shell, content *bufio.Reader) string {
+	cid, err := sh.Add(content)
 	if err != nil {
 		panic(fmt.Errorf("failed: %s", err))
 	}
 
 	fmt.Printf("successfully added file: '%s' to IPFS w/ a CID of: %s\n", "./hello", cid)
 
-	content, err := sh.Cat(cid)
+	return cid
+}
+
+func get(path string) *bufio.Reader {
+	file, err := os.Open(path)
 	if err != nil {
 		panic(fmt.Errorf("failed: %s", err))
 	}
 
+	return bufio.NewReader(file)
+}
+
+func read(content io.ReadCloser) []byte {
 	bodyBytes, err := ioutil.ReadAll(content)
 	if err != nil {
 		panic(fmt.Errorf("failed: %s", err))
 	}
 
-	content.Close()
+	return bodyBytes
+}
 
-	fmt.Printf("%s\n", bytes.NewBuffer(bodyBytes))
+func write(path string, bytes []byte, perm fs.FileMode) {
+	err := ioutil.WriteFile(path, bytes, perm)
+	if err != nil {
+		panic(fmt.Errorf("failed: %s", err))
+	}
+}
 
-	// fmt.Printf("contents contain: %s\n", new)
+func cat(sh *shell.Shell, cid string) []byte {
+	result, err := sh.Cat(cid)
+	if err != nil {
+		panic(fmt.Errorf("failed: %s", err))
+	}
+
+	bytes := read(result)
+
+	result.Close()
+
+	return bytes
+}
+
+func main() {
+	sh := shell.NewShell("localhost:5001")
+
+	path := "./data/directory/picture.png"
+
+	// get() - get any given content from unixfs
+	content := get(path)
+
+	// add() - add any given content to IPFS via the shell API
+	cid := add(sh, content)
+
+	// cat() - display contents of any given content via it's CID via the shell API
+	bytes := cat(sh, cid)
+
+	fmt.Printf("%s\n", bytes)
+
+	// write() - write any given content to any given dir
+	write("./test.png", bytes, 0644)
 }
